ada1b4c47bba12835b9c274a14b15c6c1ccc6ebe
Waqas Nazir <email.waqasnazir@gmail.com>
2019-07-07 14:32:41 -0400

    Added env file and removed useless code

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ let moreLink = '';
 
 //set db configuration
 var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'scrap_jobs'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 con.connect(function(err) {
